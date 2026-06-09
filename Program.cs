ec2f32ec3c5d7a934649f9dfffd805911a46ece6
Thijmen <thijmen.walter@hva.nl>
2026-06-09 20:50:35 +0200

    feat: :sparkles: add swagger docs capability

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo {
+         Title = "TaskManager API",
+         Description = "For managing your tasks",
+         Version = "v1" });
+});
 
 // Add services to the container.
 
@@ -12,6 +23,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskManager API V1");
+    });
 }
 
 app.UseHttpsRedirection();
